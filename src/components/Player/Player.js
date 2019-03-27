91133159c13a2a8509a851e6ba607c1478770378
Krystian <kordaszewskikrystian@gmail.com>
2019-03-27 17:31:28 +0100

    filteretWithoutState

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,33 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Player.css'
 
-import PlaylistContext from '../../data/PlaylistContext'
+import Context from '../../data/PlaylistContext'
 
 import Details from './Details'
 import Controls from './Controls'
 
 const Player = () => {
+	const { playlists, songId } = useContext(Context)
+
+	let filteredData = playlists.filter((item) => item.id === songId)[0]
+
 	return (
 		<div className="song-info ">
 			<div className="song-info-bg2" />
-			<PlaylistContext.Consumer>
-				{({ playlists, songId }) => {
-					let filteredData = playlists.filter((item) => item.id === songId)[0]
-
-					return (
-						<>
-							<div
-								className="song-info-bg"
-								style={{
-									background: `url('${filteredData.photo.url}' )bottom / cover no-repeat`
-								}}
-							/>
-							<Details filteredData={filteredData} />
-							<Controls />
-						</>
-					)
+			<div
+				className="song-info-bg"
+				style={{
+					background: `url('${filteredData.photo.url}' )bottom / cover no-repeat`
 				}}
-			</PlaylistContext.Consumer>
+			/>
+			<Details filteredData={filteredData} />
+			<Controls />
 		</div>
 	)
 }
--- src/data/GlobalState.js
+++ src/data/GlobalState.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react'
 
-import { Reducer, CHANGE_ID, CHANGE_CONTROL_STATUS } from './Reducer'
+import { Reducer, CHANGE_ID, CHANGE_CONTROL_STATUS /*FILTERED_DATA*/ } from './Reducer'
 
 import PlaylistContext from './PlaylistContext'
 
@@ -8,9 +8,15 @@ const GlobalState = (props) => {
 	const inicialState = {
 		songId: props.data.playlists[0].id,
 		isPlay: false
+		//filteredData: props.data.playlists[0]
 	}
 
 	const [playerState, dispatch] = useReducer(Reducer, inicialState)
+
+	/*const filteredData = props.data.playlists.filter((item) => item.id === playerState.songId)[0]
+	const changeFilter = () => {
+		dispatch({ type: FILTERED_DATA, filteredData })
+	}*/
 
 	const changeId = (songId) => {
 		dispatch({ type: CHANGE_ID, songId })
@@ -28,6 +34,8 @@ const GlobalState = (props) => {
 				changeId,
 				isPlay: playerState.isPlay,
 				changeControlStatus
+				//filteredData: playerState.filteredData,
+				//changeFilter
 			}}
 		>
 			{props.children}
